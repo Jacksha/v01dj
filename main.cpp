c84f1c1e16f4f15f8e7830314f9b15dd5335b2df
Dario Jakovljević <jakovljevic.dario@gmail.com>
2024-04-08 18:55:27 +0200

    Dugo mi je trebalo da da se uhodam i sve upogonim. Nisam kod vas slušao objektno (priznalo mi se još iz prahistorijskih vremena). Nadam se da ćete mi uvažiti

--- main.cpp
+++ main.cpp
@@ -4,12 +4,22 @@ enum { id_button1 = 1, id_button2 };
 
 void on_create(HWND hw) 
 {
-	// TODO: create two child windows of type button
+	CreateWindow("Button", "one", WS_CHILD | WS_VISIBLE, 85, 70, 120, 40, hw, (HMENU)id_button1, 0, 0);
+	CreateWindow("Button", "two", WS_CHILD | WS_VISIBLE, 85, 130, 120, 40, hw, (HMENU)id_button2, 0, 0);
 }
 
 void on_command(HWND hw, int id) 
 {
-	// TODO: show message box with text depending on which button was pressed
+	switch (id) {
+
+	case id_button1:
+		MessageBox(hw, "one", "NWP", MB_OK | MB_ICONWARNING);
+		break;
+
+	case id_button2:
+		MessageBox(hw, "two", "NWP", MB_OK | MB_ICONWARNING);
+		break;
+	}
 }
 
 void on_destroy() 
@@ -43,7 +53,7 @@ int register_class(HINSTANCE hi, const char* name)
 	wc.hInstance = hi;
 	wc.style = CS_HREDRAW | CS_VREDRAW;
 	wc.hCursor = ::LoadCursor(0, IDC_ARROW);
-	wc.hbrBackground = static_cast<HBRUSH>(::GetStockObject(WHITE_BRUSH));  // TODO: replace with cyan background
+	wc.hbrBackground = static_cast<HBRUSH>(::CreateSolidBrush(RGB(0, 255, 255)));
 	return ::RegisterClass(&wc);
 }
 
@@ -53,7 +63,7 @@ int WINAPI WinMain(HINSTANCE hi, HINSTANCE, LPSTR cmd_line, int show_flag)
 	if (!register_class(hi, class_name))
 		return 0;
 	::CreateWindow(class_name, "NWP 1", WS_OVERLAPPEDWINDOW | WS_VISIBLE,
-		CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, 0, 0, hi, 0);
+		1000, 500, 300, 300, 0, 0, hi, 0);
 	MSG msg;
 	while (::GetMessage(&msg, 0, 0, 0))
 		::DispatchMessage(&msg);
